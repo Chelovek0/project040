daa5c7278e1f12232882082c4b8e8ed5dcbfc119
Inf1nity <fvendetta311@gmail.com>
2024-03-29 03:10:51 +0300

    пофиксил баг с отправкой post-запроса на сервер

--- app/src/main/java/com/example/app/User.kt
+++ app/src/main/java/com/example/app/User.kt
@@ -1,20 +1,27 @@
 package com.example.app
 
-
+import android.util.Log
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import org.json.JSONObject
 import java.io.IOException
+
 class User(val userName: String, val password: String) {
     fun sendPostReg(userName: String, password: String, callback: (String) -> Unit) {
-        val url = "http://localhost:4444/register"
+        val url = "http://platwa-server.ru:32673/register"
         val client = OkHttpClient()
-        val requestBody: RequestBody = FormBody.Builder()
-            .add("userName", userName)
-            .add("password", password)
-            .build()
+
+        val json = JSONObject()
+        json.put("email", userName)
+        json.put("password", password)
+
+        val requestBody: RequestBody = RequestBody.create("application/json".toMediaTypeOrNull(), json.toString())
+
         val request: Request = Request.Builder()
             .url(url)
             .post(requestBody)
             .build()
+
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 callback("Request failed: ${e.message}")
@@ -24,8 +31,6 @@ class User(val userName: String, val password: String) {
                 val responseData: String = response.body?.string() ?: "Empty response"
                 callback(responseData)
             }
-
         })
     }
 }
-
